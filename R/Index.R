c9d22d93cd5954fe947b13a2bdf5cf2e43e5c030
Thomas Klebel <tklebel@users.noreply.github.com>
2014-11-11 00:22:19 +0100

    adding examples and optimization of funcion "Index"

--- R/Index.R
+++ R/Index.R
@@ -8,33 +8,49 @@
 #' @keywords mean-index
 #' @export
 #' @examples
-#' examples to insert
+#' # generate data
+#' set.seed(1234)
+#' item1 <- rnorm(10, 2.5, .5)
+#' item1[sample.int(length(item1), 2)] <- NA_real_
+#' item2 <- rnorm(10, 3, .5)
+#' item2[sample.int(length(item2), 3)] <- NA_real_
+#' item3 <- rnorm(10, 2.2, .5)
+#' item4 <- rnorm(10, 2, .5)
+#' df1 <- data.frame(item1, item2, item3, item4)
+#' df1
+#' # compute different Indizes
+#' Index(df1, type="mean", min=3)
+#' Index(df1, type="mean", min=2)
+#' Index(df1, type="sum", max=0)
+ 
 
-Index <- function(items, type=c("mean", "sum"), maximum, minimum) {
+
+Index <- function(items, type, max, min) {
   if(!is.data.frame(items))
     stop("Data must be a data frame")
-  if(missing(maximum) && missing(minimum))
-    stop("Either a maximum value of NAs or a minimum value of Items must be given")
-  
-  if(missing(maximum)){
-    max <- dim(items)[2]-minimum
-  } 
-  if(missing(minimum)){
-    max <- maximum
+  if(missing(max) && missing(min)) {
+    m <- 0
+    print("minimum value of items was set to 0")
   }
+  else if(missing(max)){
+    m <- dim(items)[2]-min
+  } 
+  else {m <- max}
   
   df <- items # give new name to dataframe, in order to change it
   if (!is.numeric(df[,1:ncol(df)])) { # coerce to numeric if not numeric
     df <- sapply(df, function(x) as.numeric(x) )
     df <- data.frame(df)
   }
-  df[rowSums(is.na(df)) > max,]<-NA # set all lines to NA where the number of NAs is above the maximum of allowed NAs
+  df[rowSums(is.na(df)) > m,]<-NA # set all lines to NA where the number of NAs is above the maximum of allowed NAs
   
-  if(type=="mean") {
-    skala <- rowMeans(df, na.rm=T) # compute the meanIndex
-  }
-  if(type=="sum") {
-    skala <- rowSums(df) # compute the sumIndex
-  }
+  
+  switch(type,
+         mean = {
+           skala <- rowMeans(df, na.rm=T) # compute the meanIndex
+         },
+         sum = {
+           skala <- rowSums(df) # compute the meanIndex
+         })
   return(skala)
 }
